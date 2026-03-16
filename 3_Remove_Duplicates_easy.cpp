46ec91bca6e3a87f1727392f00c6deb780543fd0
PanditAkshay402 <PanditAkshay402@gmail.com>
2026-03-16 22:57:02 +0530

    #

--- 3_Remove_Duplicates_easy.cpp
+++ 3_Remove_Duplicates_easy.cpp
@@ -1,4 +1,5 @@
-/* 83. Remove Duplicates from Sorted List
+/* 
+83. Remove Duplicates from Sorted List
 Given the head of a sorted linked list, delete all duplicates such that 
 each element appears only once. Return the linked list sorted as well.
 
@@ -13,4 +14,5 @@ Output: [1,2,3]
 Constraints:
 The number of nodes in the list is in the range [0, 300].
 -100 <= Node.val <= 100
-The list is guaranteed to be sorted in ascending order. */
+The list is guaranteed to be sorted in ascending order. 
+*/
