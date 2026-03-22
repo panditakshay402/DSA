a7c227f620ec3f1375ef03884511a44f88446d51
PanditAkshay402 <PanditAkshay402@gmail.com>
2026-03-23 02:28:30 +0530

    reverseNum

---
+++ 0_ReverseNumber.cpp
@@ -0,0 +1,43 @@
+
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+vector<int> reverseNum(const vector<int> &v)
+{
+     vector<int> result;
+    for (int i = 0; i < v.size(); i++)
+    {
+        int ognum = v[i];
+        int rev = 0;
+        while (ognum > 0)
+        {
+            int digit = ognum % 10;
+            rev = (rev * 10) + digit;
+            ognum = ognum / 10;
+        }
+        // cout<<rev<<endl;
+        result.push_back(rev);
+    }
+    return result;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    vector<int> v;
+    for (int i = 0; i < t; i++)
+    {
+        int x = 0;
+        cin >> x;
+        v.push_back(x);
+    }
+   vector<int> ans=reverseNum(v);
+
+    for(int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << "\n";
+    }
+}
