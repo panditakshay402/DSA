f28f3a41562d697786995eef20e1b9078db2708d
PanditAkshay402 <PanditAkshay402@gmail.com>
2026-03-24 23:45:00 +0530

    4. Squaring sorted array

---
+++ 4_Squaring_Sorted_Array.cpp
@@ -0,0 +1,56 @@
+/* 977. Squares of a Sorted Array
+Easy
+Topics
+premium lock icon
+Companies
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+ 
+
+Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach? */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> sortedSquares(vector<int>& nums) {
+    vector<int> ans;
+
+    // Step 1: square each element
+    for (int i = 0; i < nums.size(); i++) {
+        ans.push_back(nums[i] * nums[i]);
+    }
+
+    // Step 2: sort the result
+    sort(ans.begin(), ans.end());
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {-4,-1,0,3,10};
+
+    vector<int> result = sortedSquares(nums);
+
+    for (int x : result) {
+        cout << x << " ";
+    }
+}
