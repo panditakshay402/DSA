71d24b13af3b129d31fefa0c510224128fa7ee2a
PanditAkshay402 <PanditAkshay402@gmail.com>
2026-03-30 04:57:15 +0530

    anagram code

---
+++ 0_Anagram.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+bool anagram(string s1, string s2)
+{
+    if (s1.length() != s2.length())
+
+        return 0;
+
+    int freq[26] = {0};
+
+    for (int i = 0; i < s1.length(); i++)
+    {
+        freq[s1[i] - 'a']++;
+        freq[s2[i] - 'a']--;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i] != 0)
+
+            return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    string s1;
+    cin >> s1;
+    string s2;
+    cin >> s2;
+    if (anagram(s1, s2))
+    {
+        cout << "ana";
+    }
+    else
+    {
+        cout << "not ana";
+    }
+}
